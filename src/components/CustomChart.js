e4b0dfba434045440cf0179f21666f8d1eceaa77
ecsunahata <ecsunahata@indracompany.com>
2020-03-12 17:54:52 -0500

    fourth commit

--- src/components/CustomChart.js
+++ src/components/CustomChart.js
@@ -5,16 +5,11 @@ import 'chartjs-plugin-piechart-outlabels';
 const CustomChart = ({ customDataChart }) => {
   return (
     <div className="row customChart">
-
-      <div className="col-lg-12">
-				
-				<div className="col-lg-12">
-          <Doughnut 
-            data={customDataChart.dataChart} 
-            options = {customDataChart.params}
-          />
-				</div>
-
+      <div className="col-lg-12">		
+        <Doughnut 
+          data={customDataChart.dataChart} 
+          options = {customDataChart.options}
+        />
       </div>
 
     </div>
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -14,65 +14,76 @@ export const getRandomInt = (min, max)  => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/*
-// some of this code is a variation on https://jsfiddle.net/cmyker/u6rr5moq/
-var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
-Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
-  draw: function() {
-    originalDoughnutDraw.apply(this, arguments);
-    
-    const chart = this.chart;
-    const width = chart.width;
-    const height = chart.height;
-    const ctx = chart.ctx;
+function getOrientation() {
+  let windowScreen = window.screen;
+  let orientation = '';
+  if (windowScreen.height > windowScreen.width) {
+     orientation = 'portrait';
+  } else {
+     orientation = 'landscape';
+  }
+  return orientation;
+}
 
-    var fontSize = (height / 220).toFixed(2);
-    ctx.font = `${fontSize}em sans-serif`;
-    ctx.textBaseline = "middle";
+function doOnOrientationChange()
+{
+  var windowScreen = window.screen;
+  var orientation = '';
 
-    var sum = 0;
-    for (var i = 0; i < chart.config.data.datasets[0].data.length; i++) {
-      console.log(chart.config.data.datasets[0].data[i]);
-      sum += chart.config.data.datasets[0].data[i];
+  window.addEventListener("orientationchange", function() {
+    console.log("the orientation of the device is now " + windowScreen.orientation.angle);
+    if (windowScreen.height > windowScreen.width) {
+       console.log('portrait');
+       orientation = 'portrait';
+    } else {
+       console.log('landscape');
+       orientation = 'landscape';
     }
+  });
 
-    var min = Math.min.apply( Math, chart.config.data.datasets[0].data );
+  return orientation;
+}
 
-    const text = `${min} MB`;
-    const textX = Math.round((width - ctx.measureText(text).width) / 2);
-    const textY = height / 2;
+function setChartMiddleText (txtToShow) {
+  // Se configura un plugin para poder agregar el texto al centro del Doughnut Chart
+  var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
+  Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
+    draw: function() {
+      originalDoughnutDraw.apply(this, arguments);
+      
+      var chart = this.chart.chart;
+      var ctx = chart.ctx;
+      var width = chart.width;
+      var height = chart.height;
+      var legends = chart.legend; //Si la leyenda se ubica a la derecha, se debe restar
+      
+      var fontSize = (height / 340).toFixed(2);
+      ctx.font = fontSize + "em Verdana";
+      ctx.textBaseline = "middle";
 
-    ctx.fillText(text, textX, textY);
-  }
-});
-*/
-
-// Se configura un plugin para poder agregar el texto al centro del Doughnut Chart
-var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
-Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
-  draw: function() {
-    originalDoughnutDraw.apply(this, arguments);
-    
-    var chart = this.chart.chart;
-    var ctx = chart.ctx;
-    var width = chart.width;
-    var height = chart.height;
-    var legends = chart.legend; //Si la leyenda se ubica a la derecha, se debe restar
-
-    var fontSize = (height / 180).toFixed(2);
-    ctx.font = fontSize + "em Verdana";
-    ctx.textBaseline = "middle";
-
-    //Ubicar texto en medio del chart
-    var text = chart.config.data.text;
-    var textX = Math.round((width - ctx.measureText(text).width) / 2) - Math.round(legends.width/2);
-    var textY = height * 1 / 2; // Se multiplica si la leyenda se ubica en 'top' o 'bottom'
-    
-    ctx.fillText(text, textX, textY);
-  }
-});
+      //Ubicar texto en medio del chart
+      var text = chart.config.data.text;
+      var textX = Math.round((width - ctx.measureText(text).width) / 2) - Math.round(legends.width/2);
+      var textY = height * 1 / 2; // Se multiplica si la leyenda se ubica en 'top' o 'bottom'
+      
+      ctx.fillText(txtToShow, textX, textY);
+    }
+  });
+}
 
 export const generateDataChart = (labels, numbers, colors)  => {
+  console.log(getOrientation());
+  console.log(doOnOrientationChange());
+
+  setChartMiddleText('Chart M');
+  let paddingTop = 0;
+  let paddingBottom = 0;
+
+  if(getOrientation() === 'landscape' || doOnOrientationChange() === 'landscape') {
+    paddingTop = 55;
+    paddingBottom = 55;
+  }
+
   let dataChart = {
     labels: labels,
     datasets: [
@@ -84,7 +95,7 @@ export const generateDataChart = (labels, numbers, colors)  => {
     text: 'Texto X'
   }
 
-  var params = {
+  var options = {
     animation: {
         duration: 2000,
     },
@@ -106,16 +117,22 @@ export const generateDataChart = (labels, numbers, colors)  => {
       padding: {
         left: 0,
         right: 0,
-        top: 55,
-        bottom: 55,
+        top: paddingTop,
+        bottom: paddingBottom
       },
     },
     legend: { 
       display: true ,
       position: 'right'
+    },
+    plugins: {
+      outlabels:{
+        // text: '%l PER => %p \n VAL => %v'
+        text: '%v'
+      }
     }
   }
 
-  var data = {dataChart, params}
+  var data = {dataChart, options}
   return data;
 }
